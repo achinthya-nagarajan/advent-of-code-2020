a250d8ba5b2d574cf34b0af3b04dc4c6e788101a
Reagan Chisholm <reaganchisholm@gmail.com>
2020-12-06 13:05:25 -0500

    Complete Day 5 Part 1

--- day5/day5.py
+++ day5/day5.py
@@ -1,57 +1,62 @@
-import math
-
 boarding_pass_raw = open('puzzle_input.txt', 'r').read().split('\n')
 
 def get_seat_ids(boarding_passes):
     seat_ids = []
 
     for bp in boarding_passes:
-        break
         row_min_num = 0
-        row_max_num = 127
+        row_max_num = 128
         row = 0
         col_min_num = 0
-        col_max_num = 7
+        col_max_num = 8
         col = 0
 
         for i in range(7):
+            rows_left = list(range(row_min_num, row_max_num))
+            row_mid_point = len(rows_left) // 2
             if(i != 6):
                 if bp[i] == 'F':
-                    num = math.floor(((row_max_num - row_min_num) / 2))
-                    row_max_num = row_max_num - num
+                    # lower half
+                    row_max_num = row_max_num - row_mid_point
                 elif bp[i] == 'B':
-                    num = math.ceil((row_max_num - row_min_num) / 2)
-                    row_min_num = row_min_num + num
+                    # upper half
+                    row_min_num = row_min_num + row_mid_point 
             else:
                 if bp[i] == 'F':
-                    row = row_min_num
+                    # lower of the two
+                    row = rows_left[0]
                 elif bp[i] == 'B':
-                    row = row_max_num
-        for i in range(4):
-            if(i != 3):
+                    # higher of the two
+                    row = rows_left[1]
+        for i in range(3):
+            cols_left = list(range(col_min_num, col_max_num))
+            col_mid_point = len(cols_left) // 2
+            if(i != 2):
                 if bp[7 + i] == 'L':
-                    num = math.floor((col_max_num - col_min_num) / 2)
-                    col_max_num = col_max_num - num
+                    # lower half
+                    col_max_num = col_max_num - col_mid_point
                 if bp[7 + i] == 'R':
-                    num = math.ceil((col_max_num - col_min_num) / 2)
-                    col_min_num = col_min_num + num 
-            else: 
-                if bp[6 + i] == 'L':
-                    col = col_min_num
-                elif bp[6 + i] == 'R':
-                    col = col_max_num
+                    # upper half
+                    col_min_num = col_min_num + col_mid_point
+            else:
+                if bp[7 + i] == 'L':
+                    # lower of the two
+                    col = cols_left[0]
+                elif bp[7 + i] == 'R':
+                    # higher of the two
+                    col = cols_left[1]
 
-        seat_id = round(row * 8 + col)
+        seat_id = row * 8 + col
         seat_ids.append(seat_id)
-        print(f"{bp}: row {row} col {col} seat ID {round(row * 8 + col)}")
+        # print(f"{bp}: row {row} col {col} seat ID {round(row * 8 + col)}")
 
     seat_ids.sort()
     return seat_ids
 
-boarding_pass_test = ['FBFBBFFRLR', 'BFFFBBFRRR', 'FFFBBBFRRR', 'BBFFBBFRLL']
-seat_ids = get_seat_ids(boarding_pass_test)
-# seat_ids = get_seat_ids(boarding_pass_raw)
+# ----- Testing ------------------------------------
+# boarding_pass_test = ['FBFBBFFRLR', 'BFFFBBFRRR', 'FFFBBBFRRR', 'BBFFBBFRLL']
+# seat_ids = get_seat_ids(boarding_pass_test)
+# --------------------------------------------------
 
-# print(f"Part 1 -- Highest Seat ID: {seat_ids[-1]}")
-# 839 invalid answer
-# 830 invliad answer
+seat_ids = get_seat_ids(boarding_pass_raw)
+print(f"Part 1 -- Highest Seat ID: {seat_ids[-1]}")
