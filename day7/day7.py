cf8e581980e969880f276e13f91c357d375bf326
Reagan Chisholm <reaganchisholm@gmail.com>
2020-12-08 07:59:29 -0500

    Cont. Work On Day 7

--- day7/day7.py
+++ day7/day7.py
@@ -2,14 +2,31 @@ import re
 
 baggage_regulations_raw = open('puzzle_input.txt', 'r').read().split('\n')
 
-def parse_regulations(baggage_regulations):
-    regulations_tree = {}
-    for regulations in baggage_regulations_raw:
-        print(regulations)
-        match = re.findall("([a-z]+ [a-z]+) bags contain (.+)$", regulations)[0]
-        regulations_tree[match[0]] = None
-        print(match[1].split(','))
-        break
+def parse_regulation(regulation):
+    match = re.findall("([a-z]+ [a-z]+) bags contain (.+)$", regulation)[0]
+    value = match[0]
+    next_values = match[1].replace('.', '').split(',')
+    if(next_values == 'no other bags'):
+        next_values = None
 
-    print(regulations_tree)
-parse_regulations(baggage_regulations_raw)
+    return value, next_values
+
+def check_regulations(baggage_regulations):
+    regulations_tree = {}
+    for regulation in baggage_regulations_raw:
+        value, next_values = parse_regulation(regulation)
+        regulations_tree[value] = next_values
+
+    return regulations_tree
+
+regulations_tree = check_regulations(baggage_regulations_raw)
+
+def pretty_print(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty_print(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
+
+pretty_print(regulations_tree, 1)
