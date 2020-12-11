7e0618878f92d6fc8e7545973ac4b53bd4f7f4fc
Reagan Chisholm <reaganchisholm@gmail.com>
2020-12-10 22:41:37 -0500

    Complete Day8

--- day8/day8.py
+++ day8/day8.py
@@ -1,43 +1,78 @@
-import re
+import re, copy
 
-game_intructions = open('puzzle_input.txt', 'r').read().split('\n')
+game_instructions = []
 
-lines_run = []
+with open('puzzle_input.txt') as puzzle_input:
+    for line in puzzle_input:
+        line = line.replace('\n', '')
+        operation = line[:3]
+        amount = int(line[4:])
+        instruction = [operation, amount, False]
+        game_instructions.append(instruction)
 
-def run_line(line, index):
+def run(instructions):
+    copy_of_instructions = copy.deepcopy(instructions)
+    current_line = 0 
     accumulator = 0
-    current_line = index
+    game_terminates = False
 
-    if current_line in lines_run:
-        print(f'Hit a line we already ran {current_line}')
-        return
-    else:
-        lines_run.append(current_line) 
+    while True:
+        if(current_line >= len(copy_of_instructions)):
+            #If our game gets to one line after our game, it terminated properly
+            game_terminates = True
+            break
+        
+        operation = copy_of_instructions[current_line][0]
+        amount = copy_of_instructions[current_line][1]
+        instruction_ran_already = copy_of_instructions[current_line][2]
 
-        parse_line = re.findall('(nop|acc|jmp) (\+|-)(\d+)', line)[0]
-        action = parse_line[0]
-        symbol = parse_line[1]
-        amount = int(parse_line[2])
+        if(instruction_ran_already):
+            # Woops, we looping!
+            break
+        else:
+            # Mark the line as ran already to prevent loops
+            copy_of_instructions[current_line][2] = True
 
-        if(action == 'nop'):
-            current_line += 1
-        elif(action == 'acc'):
-            if(symbol == '-'):
-                accumulator -= amount
+            if(operation == 'nop'):
                 current_line += 1
-            elif(symbol == '+'):
+            elif(operation == 'acc'):
                 accumulator += amount
                 current_line += 1
-        elif(action == 'jmp'):
-            if(symbol == '-'):
-                current_line += - amount
-            elif(symbol == '+'):
+            elif(operation == 'jmp'):
                 current_line += amount
-        
-        amount = run_line(game_intructions[current_line], current_line)
-        if isinstance(amount, int):
-            accumulator += amount
-    return accumulator
+            
+    return (accumulator, game_terminates)
 
-accumulator = run_line(game_intructions[0], 0)
-print(f"Part 1 -- {accumulator}")
+def find_all_nop_and_jmp_indexes(instructions):
+    all_nop_and_jmps_indexes = []
+    for i, line in enumerate(game_instructions):
+        operation = line[0]
+        if operation == 'nop' or operation == 'jmp':
+            all_nop_and_jmps_indexes.append(i)
+    return all_nop_and_jmps_indexes
+
+def find_terminating_game(indexes, game_instructions):
+    terminated_result = None
+    for index in indexes:
+        copy_of_game_instructions = copy.deepcopy(game_instructions)
+        if(copy_of_game_instructions[index][0] == 'nop'):
+            copy_of_game_instructions[index][0] = 'jmp'
+            game_result = run(copy_of_game_instructions)
+            if(game_result[1] == True):
+                terminated_result = game_result 
+                break
+        if(copy_of_game_instructions[index][0] == 'jmp'):
+            copy_of_game_instructions[index][0] = 'nop'
+            game_result = run(copy_of_game_instructions)
+            if(game_result[1] == True):
+                terminated_result = game_result 
+                break
+    return terminated_result
+
+p1_results = run(game_instructions)
+
+p2_indexes = find_all_nop_and_jmp_indexes(game_instructions)
+p2_results = find_terminating_game(p2_indexes, game_instructions)
+
+print(f"Part 1 -- {p1_results[0]}") #1709
+print(f"Part 2 -- {p2_results[0]}")
