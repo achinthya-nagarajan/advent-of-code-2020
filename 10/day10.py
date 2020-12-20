0e931f438d1408dad8a6a6a27aaf34c6ce9e7c2b
Reagan Chisholm <reaganchisholm@gmail.com>
2020-12-19 21:18:44 -0500

    Cleaning up day10

--- 10/day10.py
+++ 10/day10.py
@@ -1,26 +1,13 @@
 joltage_raw = open('puzzle_input.txt', 'r').read().split('\n')
 
-joltage_test_small = """16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4""".split('\n')
-
 converted_voltage = []
-
 for j in joltage_raw:
     converted_voltage.append(int(j))
-    
 converted_voltage.sort()
-differences = [0, 0, 0]
 
-# Add our first difference
+
+#Part One
+differences = [0, 0, 0]
 differences[converted_voltage[0] - 1] += 1
 
 for i, v in enumerate(converted_voltage):
@@ -34,3 +21,18 @@ for i, v in enumerate(converted_voltage):
 
 print(f"Part 1: {differences[0] * differences[2]}")
 
+
+# Part Two
+adapter_paths = {}
+adapter_paths[0] = 1
+for adapter in converted_voltage:
+    amount_of_paths = 0
+    for n in (1, 2, 3):
+        # print(f"{n} ---> {adapter_paths.get(adapter - n, 0)}")
+        amount_of_paths += adapter_paths.get(adapter - n, 0)
+    # print(f"Amount of possible paths: {paths_to}")
+    adapter_paths[adapter] = amount_of_paths
+    # print(f"-------------------------")
+    # print(f"{adapter}: {adapter_paths}")
+
+print(f"Part 2: {adapter_paths[converted_voltage[-1]]}")
