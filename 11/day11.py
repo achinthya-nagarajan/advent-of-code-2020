55af466d08ec11d4f54d6e1439a7a026f7e1a1c8
Reagan Chisholm <reaganchisholm@gmail.com>
2020-12-27 20:02:07 -0500

    Complete Day 11 Part 1

--- 11/day11.py
+++ 11/day11.py
@@ -1,3 +1,10 @@
+import copy
+
+flatten = lambda t: [item for sublist in t for item in sublist]
+def pp(LIST):
+    for l in LIST:
+        print(l)
+
 seats_raw = open('puzzle_input.txt').read()
 
 seats_test = """L.LL.LL.LL
@@ -13,12 +20,63 @@ L.LLLLL.LL"""
 
 seat_array = []
 
-for l in seats_test.split('\n'):
-    seat_array.append(l)
+for l in seats_raw.split('\n'):
+    seat_line = []
+    for c in l:
+        seat_line.append(c)
+    seat_array.append(seat_line)
 
-for seat_line in seat_array:
-    for seat in seat_line:
-        seat_status = 0
-            for seat_line in seat_array:
-                for seat in seat_line:
+copy_of_seats = copy.deepcopy(seat_array)
+something_changed = True
+amount_of_changes = 0
 
+while something_changed == True:
+    if(something_changed == False):
+        break
+    for line_index, seat_line in enumerate(seat_array):
+        for seat_index, seat in enumerate(seat_line):
+            if(seat == '.'):
+                continue
+            adjacent_seats = [
+                ["", "", ""],
+                ["", "[X]", ""],
+                ["", "", ""]
+            ]
+
+            if(line_index != 0):
+                if(seat_index != 0):
+                    adjacent_seats[0][0] = seat_array[line_index - 1][seat_index - 1]
+                adjacent_seats[0][1] = seat_array[line_index - 1][seat_index]
+                if(seat_index + 1 != len(seat_line)):
+                    adjacent_seats[0][2] = seat_array[line_index - 1][seat_index + 1]
+            if(seat_index != 0):
+                adjacent_seats[1][0] = seat_array[line_index][seat_index - 1]
+            if(seat_index + 1 != len(seat_line)):
+                adjacent_seats[1][2] = seat_array[line_index][seat_index + 1]
+            if(line_index + 1 != len(seat_array)):
+                if(seat_index != 0):
+                    adjacent_seats[2][0] = seat_array[line_index + 1][seat_index - 1]
+                adjacent_seats[2][1] = seat_array[line_index + 1][seat_index]
+                if(seat_index + 1 != len(seat_line)):
+                    adjacent_seats[2][2] = seat_array[line_index + 1][seat_index + 1]
+
+            flattened = flatten(adjacent_seats)
+
+            if(seat == 'L'):
+                if('#' not in flattened):
+                    copy_of_seats[line_index][seat_index] = '#'
+            elif(seat == '#'):
+                if(flattened.count('#') >= 4):
+                    copy_of_seats[line_index][seat_index] = 'L'
+    amount_of_changes += 1
+
+    # print(f"LAYOUT: {amount_of_changes} ----------------------------")
+    # pp(copy_of_seats)
+
+    if(seat_array == copy_of_seats):
+        something_changed = False
+    else:
+        seat_array = copy.deepcopy(copy_of_seats)
+
+occupied_seats = flatten(seat_array).count('#')
+print(f"""Part 1 Answer: {occupied_seats}""")
